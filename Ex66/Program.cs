0841f624fcd922b25dc6af45dcc91358aeb46fc2
anna <a1b2v3@mail.ru>
2023-01-09 20:19:02 +0500

    add Ex64,66,68

---
+++ Ex66/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int Naturalnumber(int M, int N)
+{
+
+
+if (M==N) return M;
+else return M + Naturalnumber (M+1, N);
+
+
+}
+Console.WriteLine(Naturalnumber(2, 3));
